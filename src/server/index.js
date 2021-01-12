20a9225ac81b59ca3e148e7b6293953928ab3ade
MustafaColakoglu <mustafacolakoglu7@gmail.com>
2021-01-12 17:37:24 +0300

    node js file modified

--- src/server/index.js
+++ src/server/index.js
@@ -1,29 +1,23 @@
 var app=require("express")();
 var http=require("http").createServer(app);
-var io=require("socket.io")(http);
 var cors = require('cors')
 app.use(cors())
 
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
-   // Add this
-   if (req.method === 'OPTIONS') {
-  
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
-        res.header('Access-Control-Max-Age', 120);
-        return res.status(200).json({});
+const io = require("socket.io")(http, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+ 
     }
-  
-    next();
-  
   });
+
+
 
 app.get("/",function(req,res){
     res.send("<h1>Hello World</h1>")
 });
+
+
 
 io.on("connection",function(socket){
     console.log("a user connected")
